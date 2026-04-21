d17a9c0d9daaece86299bd44cc50056d31d29998
Vrishab17 <chettyvrishi@gmail.com>
2026-04-21 16:37:40 +1200

    Stop tracking Next.js build and dependency files

--- .next/dev/static/chunks/src_app_layout_tsx_1cf6b850._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__ea38df25._.css"
-],
-    source: "dynamic"
-});
