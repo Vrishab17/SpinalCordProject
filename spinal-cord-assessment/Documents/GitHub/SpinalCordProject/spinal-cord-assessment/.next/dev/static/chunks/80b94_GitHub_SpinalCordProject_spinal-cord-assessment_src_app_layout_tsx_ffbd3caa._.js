6d71f852b0a1a72b21819b527b466b61dfef0009
Max-Walka <walkermax193@gmail.com>
2026-05-01 14:06:24 +1200

    cleanup: removed .next and cache files

--- spinal-cord-assessment/Documents/GitHub/SpinalCordProject/spinal-cord-assessment/.next/dev/static/chunks/80b94_GitHub_SpinalCordProject_spinal-cord-assessment_src_app_layout_tsx_ffbd3caa._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__7ca3c78f._.css"
-],
-    source: "dynamic"
-});
